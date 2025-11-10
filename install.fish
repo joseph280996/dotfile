9ea314ae9f0f5a52fe7f2c635295cedf3aa86b85
Tung Pham <joseph.pham280996@gmail.com>
2025-11-09 23:32:46 -0500

    feat: add mcphub + nvm + install with fish script

---
+++ install.fish
@@ -0,0 +1,58 @@
+#!/usr/bin/env fish
+
+echo "Started Installing Configurations"
+
+echo "Started Detecting OS Type..."
+set uname_output (uname)
+
+switch $uname_output
+case "Linux"
+  # Check for WSL using multiple methods
+  if test -f /proc/sys/kernel/osrelease; and grep -qi wsl /proc/sys/kernel/osrelease 2>/dev/null
+    if grep -qi arch /etc/os-release 2>/dev/null
+      echo "Arch WSL Detected..."
+      if test ! -d "~/.config" 
+        echo ".config folder does not exist!"
+        echo "Started Linking .config Folder..."
+        ln "$CONFIG_DOWNLOAD_LOCATION/.config" ~/.config
+        echo "Finished Linking .config Folder..."
+      end
+    else
+      echo "Linux WSL Environment Detected..."
+      if test ! -d "~/.config" 
+        echo ".config folder does not exist!"
+        echo "Started Linking .config Folder"
+        ln "$CONFIG_DOWNLOAD_LOCATION/.config" ~/.config
+        echo "Finished Linking .config Folder"
+      end
+    end
+  else if test -f /etc/os-release; and grep -q arch /etc/os-release 2>/dev/null
+    echo "Arch Linux Detected..."
+    if test ! -d "~/.config" 
+      echo ".config folder does not exist!"
+      echo "Started Linking .config Folder"
+      ln "$CONFIG_DOWNLOAD_LOCATION/.config" ~/.config
+      echo "Finished Linking .config Folder"
+    end
+  else
+    echo "Linux Detected..."
+    if test ! -d "~/.config" 
+      echo ".config folder does not exist!"
+      echo "Started Linking .config Folder"
+      ln "$CONFIG_DOWNLOAD_LOCATION/.config" ~/.config
+      echo "Finished Linking .config Folder"
+    end
+  end
+case "Darwin"
+  echo "MacOS Detected..."
+  if test ! -d "~/.config" 
+    echo ".config folder does not exist!"
+    echo "Started Linking .config Folder"
+    ln "$CONFIG_DOWNLOAD_LOCATION/.config" ~/.config
+    echo "Finished Linking .config Folder"
+  end
+
+case "*"
+  # Fish is not supported on Windows
+  echo "unknown"
+end
